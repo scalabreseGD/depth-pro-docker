c99d47762c4068f9b1feead09876a1619f379561
scalabrese <scalabrese@griddynamics.com>
2024-11-20 19:32:45 +0100

    First commit

--- get_pretrained_models.sh
+++ get_pretrained_models.sh
@@ -3,6 +3,6 @@
 # For licensing see accompanying LICENSE file.
 # Copyright (C) 2024 Apple Inc. All Rights Reserved.
 #
-mkdir -p checkpoints
+mkdir -p app/checkpoints
 # Place final weights here:
-wget https://ml-site.cdn-apple.com/models/depth-pro/depth_pro.pt -P checkpoints
+wget https://ml-site.cdn-apple.com/models/depth-pro/depth_pro.pt -P app/checkpoints
